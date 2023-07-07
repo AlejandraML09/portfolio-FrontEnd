8e992f209a65006cb75b3433f2e125873f59ceb8
Bruno escritorio <alejandramelisalopez2@gmail.com>
2023-07-07 18:34:40 -0300

    se sube la conexión deployada

--- src/globals.ts
+++ src/globals.ts
@@ -1,4 +1,5 @@
-// export const backendUrl: string="https://portfolio-backend-y53h.onrender.com";
+// Conexión deployada:
+export const backendUrl: string="https://portfolio-backend-y53h.onrender.com";
 
 // Conexión Local:
-export const backendUrl: string="http://localhost:8080";
+// export const backendUrl: string="http://localhost:8080";
